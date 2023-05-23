270a662de179e2ca2deecddf2b8e57f2c409e7d5
John Klucinec <klucinej@oregonstate.edu>
2023-05-23 18:47:41 -0400

    Updating with the same thing

--- README.md
+++
@@ -1,5 +0,0 @@
-# Windows 10 Optimizer
-
-This is a program you can use to optimize windows 10.
-
-Version: 1.1.0-beta
--- gui.py
+++ gui.py
@@ -1,63 +1,69 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from ..code.uninstaller import get_installed_programs, call_uninstall_programs
 
-# Modes: "System" (standard), "Dark", "Light"
+# Set appearance mode and color theme
 customtkinter.set_appearance_mode("System")
-# Themes: "blue" (standard), "green", "dark-blue"
 customtkinter.set_default_color_theme("green")
-
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        # configure window
+        # Configure window
         self.title("Windows Optimizer.py")
-        self.geometry(f"{1100}x{580}")
+        self.geometry("1100x580")
 
-        # configure grid layout (4x4)
+        # Configure grid layout (4x4)
         self.grid_columnconfigure((1, 2, 3), weight=1)
-        self.grid_rowconfigure((0), weight=0)
-        self.grid_rowconfigure((1), weight=1)
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
 
-        # create topbar frame with widgets
-        self.topbar_frame = customtkinter.CTkFrame(
-            self, width=140, corner_radius=0)
+        # Create topbar frame with widgets
+        self.topbar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.topbar_frame.grid(row=0, column=0, columnspan=4, sticky="new")
         self.topbar_frame.grid_columnconfigure(0, weight=0)
         self.logo_label = customtkinter.CTkLabel(
-            self.topbar_frame, text="Windows Optimizer", font=customtkinter.CTkFont(size=20, weight="bold"))
+            self.topbar_frame,
+            text="Windows Optimizer",
+            font=customtkinter.CTkFont(size=20, weight="bold"),
+        )
         self.logo_label.grid(row=0, column=0, padx=10, pady=(5, 5))
 
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self, values=["Light", "Dark", "System"],
-                                                                       command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(
-            row=0, column=3, padx=10, pady=(5, 0), sticky="e")
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(
+            self,
+            values=["Light", "Dark", "System"],
+            command=self.change_appearance_mode_event,
+        )
+        self.appearance_mode_optionemenu.grid(row=0, column=3, padx=10, pady=(5, 0), sticky="e")
 
-        # create tabview
+        # Create tabview
         self.tabview = customtkinter.CTkTabview(self, width=950, height=550)
-        self.tabview.grid(row=1, column=0, columnspan=4,
-                          padx=(10, 10), pady=(5, 10))
+        self.tabview.grid(row=1, column=0, columnspan=4, padx=(10, 10), pady=(5, 10))
         self.tabview.add("Basic Tweaks")
         self.tabview.add("Uninstaller")
         self.tabview.add("Hardware")
         self.tabview.add("About")
 
-        # set tab columns
-        self.tabview.tab("Basic Tweaks").grid_columnconfigure(
-            3, weight=1)  # configure grid of individual tabs
+        # Set tab columns
+        self.tabview.tab("Basic Tweaks").grid_columnconfigure(3, weight=1)
         self.tabview.tab("Uninstaller").grid_columnconfigure(0, weight=1)
         self.tabview.tab("Hardware").grid_columnconfigure(0, weight=1)
         self.tabview.tab("About").grid_columnconfigure(0, weight=1)
 
         # Basic Tweaks Tab
-        # Basic Tweaks: scrollable windows tweaks
-        self.system_tweaks = customtkinter.CTkScrollableFrame(self.tabview.tab(
-            "Basic Tweaks"), label_text="System Tweaks", width=300, height=400)
+        self.system_tweaks = customtkinter.CTkScrollableFrame(
+            self.tabview.tab("Basic Tweaks"),
+            label_text="System Tweaks",
+            width=300,
+            height=400,
+        )
         self.system_tweaks.grid(row=0, column=0, padx=(20, 0), pady=(20, 0))
-        self.system_tweaks.grid_columnconfigure((0), weight=1)
-        self.system_tweaks.grid_rowconfigure((0), weight=0)
+        self.system_tweaks.grid_columnconfigure(0, weight=1)
+        self.system_tweaks.grid_rowconfigure(0, weight=0)
+
+        # Create system_tweaks_switches list
         self.system_tweaks_switches = []
         for i in range(20):
             switch = customtkinter.CTkSwitch(
@@ -65,7 +71,7 @@ class App(customtkinter.CTk):
             switch.grid(row=i, column=0, padx=10, pady=(0, 20))
             self.system_tweaks_switches.append(switch)
 
-        # Basic Tweaks: scrollable mouse tweaks
+        # Create mouse_tweaks_switches list
         self.mouse_tweaks = customtkinter.CTkScrollableFrame(self.tabview.tab(
             "Basic Tweaks"), label_text="Mouse Tweaks", width=300, height=400)
         self.mouse_tweaks.grid(row=0, column=1, padx=(20, 0), pady=(20, 0))
@@ -117,35 +123,78 @@ class App(customtkinter.CTk):
                 '<Leave>', lambda e, arg=i: tweak_hover_leave(e, arg))
 
         # Basic Tweaks: Apply Button
-        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("Basic Tweaks"), text="Apply",
-                                                           command=self.open_input_dialog_event)
-        self.string_input_button.grid(
-            row=0, column=2, padx=(10, 10), pady=(0, 0))
+        self.string_input_button = customtkinter.CTkButton(
+            self.tabview.tab("Basic Tweaks"),
+            text="Apply",
+            command=self.open_input_dialog_event,
+        )
+        self.string_input_button.grid(row=0, column=2, padx=(10, 10), pady=(0, 0))
 
         # Uninstaller Tab
-        self.label_uninstall = customtkinter.CTkLabel(
-            self.tabview.tab("Uninstaller"), text="GUI COMING SOON")
-        self.label_uninstall.grid(row=0, column=0, padx=20, pady=20)
+        self.refresh_programs_event()
+
+        # Uninstaller: Uninstall Button
+        self.uninstall_button = customtkinter.CTkButton(
+            self.tabview.tab("Uninstaller"),
+            text="Uninstall",
+            command=self.open_input_dialog_event,
+        )
+        self.uninstall_button.grid(row=0, column=1, padx=(10, 10), pady=(0, 0), sticky="e")
+
+        # Uninstaller: Refresh Button
+        self.string_input_button = customtkinter.CTkButton(self.tabview.tab("Uninstaller"), text="Refresh",
+                                                        command=self.refresh_programs_event)
+        self.string_input_button.grid(row=0, column=2, padx=(10, 10), pady=(0, 0))  
 
         # Hardware Tab
-        self.label_uninstall = customtkinter.CTkLabel(
+        # ...
+
+        self.label_hardware = customtkinter.CTkLabel(
             self.tabview.tab("Hardware"), text="GUI COMING SOON")
-        self.label_uninstall.grid(row=0, column=0, padx=20, pady=20)
-        
+        self.label_hardware.grid(row=0, column=0, padx=20, pady=20)
+            
         # About Tab
-        self.label_uninstall = customtkinter.CTkLabel(
+        # ...
+        self.label_about = customtkinter.CTkLabel(
             self.tabview.tab("About"), text="GUI COMING SOON")
-        self.label_uninstall.grid(row=0, column=0, padx=20, pady=20)
+        self.label_about.grid(row=0, column=0, padx=20, pady=20)
 
         # set default values
         self.system_tweaks_switches[0].select()
-        self.system_tweaks_switches[4].select()
+        self.system_tweaks_switches[4].select() 
         self.appearance_mode_optionemenu.set("Dark")
 
+    # Event handlers
+    #def open_input_dialog_event(self):
+        #dialog = customtkinter.CTkInputDialog(
+            #text="Type 'YES' if you want to apply the Tweaks", title="apply_tweak")
+        #print("User Response:", dialog.get_input())
     def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(
-            text="Type 'YES' if you want to apply the Tweaks", title="apply_tweak")
-        print("User Response:", dialog.get_input())
+        selected_items_array = self.get_selected_items()
+        results = call_uninstall_programs(selected_items_array)
+        #self.refresh_programs_event()
+        print (results)
+
+    def get_selected_items(self):
+        return [
+            switch._text for switch in self.uninstall_list_switches if switch.get()
+        ]
+    
+    def refresh_programs_event(self):
+        list_of_programs = get_installed_programs()
+
+        self.uninstall_list = customtkinter.CTkScrollableFrame(self.tabview.tab(
+            "Uninstaller"), label_text="Installed Programs", width=400, height=400)
+        self.uninstall_list.grid(row=0, column=0, padx=(20, 0), pady=(20, 0))
+        self.uninstall_list.grid_columnconfigure((0), weight=1)
+        self.uninstall_list.grid_rowconfigure((0), weight=0)
+        self.uninstall_list_switches = []
+
+        for i, item in enumerate(list_of_programs):
+            switch = customtkinter.CTkSwitch(
+                master=self.uninstall_list, text=f"{item}")
+            switch.grid(row=i, column=0, padx=10, pady=(0, 20), sticky="w")
+            self.uninstall_list_switches.append(switch)
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
